99c646578739f352f61241c50b25f5d190403140
Daniel Young <Danyoung91@gmail.com>
2022-11-17 13:16:48 -0500

    completed project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector("form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    createToDo(e.target.new_task_description.value);
+    form.reset();
+  });
 });
+
+function createToDo(toDo) {
+  console.log(toDo);
+  let xButton = document.createElement("button");
+  xButton.textContent = "x";
+  xButton.addEventListener("click", handelDelete);
+
+  let p = document.createElement("p");
+  p.textContent = `${toDo}  `;
+
+  p.appendChild(xButton);
+  document.querySelector("#list").appendChild(p);
+}
+
+function handelDelete(e) {
+  e.target.parentNode.remove();
+}
